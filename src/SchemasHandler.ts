cf9601cc805e0fe484c5566ac42d6103448afea9
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-06 02:58:19 +0200

    feat: implemented basic schema handler class

--- src/SchemasHandler.ts
+++ src/SchemasHandler.ts
@@ -1,20 +1,72 @@
 import { mkdirSync, readdirSync, readFileSync } from "node:fs";
-
+import { scheduler } from "node:timers/promises";
 
 type Options = {
-    migrationsPath: string | undefined;
+    migrationsPath?: string; // Without "/" at the end
 };
+
+export function isErrnoException(err: unknown) {
+    return (
+        err != null &&
+        typeof err === "object" &&
+        "errno" in err &&
+        "code" in err &&
+        "path" in err &&
+        "syscall" in err
+    );
+}
 
 class SchemasHandler {
     private readonly DEFAULT_MIGRATION_PATH = "./migrations";
-    private readonly sqlRE = /\.sql$/;
-    private opts: Options | undefined;  
-    constructor(opts: Options | undefined = undefined) {
-        this.opts = opts;
+    private opts: Options;
+    constructor(opts: Options = {}) {
+        this.opts = {
+            ...opts,
+            migrationsPath: opts.migrationsPath ?? this.DEFAULT_MIGRATION_PATH,
+        };
     }
 
-    ensureMigrationPathExists() {
-        mkdirSync(this.opts?.migrationsPath ?? this.DEFAULT_MIGRATION_PATH);
+    private ensureMigrationPathExists() {
+        try {
+            if (this.opts.migrationsPath == null) return;
+            mkdirSync(this.opts.migrationsPath);
+        } catch (err) {
+            if (isErrnoException(err)) {
+                const { errno } = err;
+                if (errno === -17) return;
+            }
+            throw err;
+        }
+    }
+    private getSchemasFilesName(): Array<string> {
+        this.ensureMigrationPathExists();
+        if (this.opts.migrationsPath == null) throw new Error("No migrations path provided");
+        const schemas = readdirSync(this.opts.migrationsPath).filter((path) =>
+            path.endsWith(".sql"),
+        );
+        return schemas;
+    }
+
+    private addMigrationPathToSchemasName(schemasFilesNames: Array<string>): Array<string> {
+        return schemasFilesNames.map((name) => this.opts.migrationsPath + "/" + name);
+    }
+
+    private readSQL(sqlPath: string) {
+        return readFileSync(sqlPath);
+    }
+
+    private combineSchemas(schemasPaths: Array<string>): string {
+        let finalSql = "";
+        for (const path of schemasPaths) {
+            finalSql += this.readSQL(path) + "\n";
+        }
+        return finalSql;
+    }
+
+    getSQLMigration() {
+        const schemasFilesNames = this.getSchemasFilesName();
+        const schemasPaths = this.addMigrationPathToSchemasName(schemasFilesNames);
+        return this.combineSchemas(schemasPaths);
     }
 }
 
