689854e78c9f9e107019f5b2af2012bb862cb619
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-07 23:05:28 +0200

    Added Config Reader class for reading config file ez-migrate.json

---
+++ src/ConfigReader.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from "node:fs";
+import { isErrnoException, throwMessage } from "./Errors";
+const Dialects = {
+    MYSQL: "mysql",
+} as const;
+
+class ConfigReader {
+    static readonly configPath = "./ez-migrate.json";
+    private config: Record<string, any>;
+    constructor() {
+        this.config = this.readConfig();
+        this.validateConfig();
+    }
+
+    private readConfig() {
+        try {
+            const json = readFileSync(ConfigReader.configPath).toString();
+            if (!json) throw new Error("Configuration file not found");
+            return JSON.parse(json);
+        } catch (err) {
+            if (isErrnoException(err)) {
+                throwMessage(err as NodeJS.ErrnoException);
+            }
+            console.error("Error reading the config file: \n", err);
+        }
+    }
+
+    private validateConfig() {
+        if (typeof this.config !== "object") throw new Error("Invalid configuration file.");
+        else if (Object.values(Dialects).includes(this.config.dialect) === false)
+            throw new Error("Invalid dialect");
+    }
+
+    getConfig() {
+        return this.config;
+    }
+}
+
+export default ConfigReader;
