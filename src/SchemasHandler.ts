9764907c1fc05911d8ea6d9219d2ec300026bda8
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-09 12:15:56 +0200

    feat: add UP/DOWN sql file split method to SchemasHandler:
    Added a method to the SchemasHandler class to split an up and down part from a sql migration file
    between comments "-- ez-migration-up" and  "-- ez-migration-down"

--- src/SchemasHandler.ts
+++ src/SchemasHandler.ts
@@ -1,24 +1,20 @@
 import { mkdirSync, readdirSync, readFileSync } from "node:fs";
 import { ERRORS, isErrnoException } from "./Errors";
-
-type Config = {
-    migrationsPath?: string; // Without "/" at the end
-};
+import ConfigReader from "./ConfigReader";
 
 class SchemasHandler {
+    private readonly upDownSeparatorRE = /^-- ez-migration-(up|down)\n/gm;
     private readonly DEFAULT_MIGRATION_PATH = "./migrations";
-    private config: Config;
-    constructor(config: Config = {}) {
-        this.config = {
-            ...config,
-            migrationsPath: config.migrationsPath ?? this.DEFAULT_MIGRATION_PATH,
-        };
+    private readonly migrationsPath: string;
+    constructor() {
+        const { migrationsPath } = ConfigReader.getConfig();
+        this.migrationsPath = migrationsPath ?? this.DEFAULT_MIGRATION_PATH;
     }
 
     private ensureMigrationPathExists() {
         try {
-            if (this.config.migrationsPath == null) return;
-            mkdirSync(this.config.migrationsPath);
+            if (this.migrationsPath == null) return;
+            mkdirSync(this.migrationsPath);
         } catch (err) {
             if (isErrnoException(err)) {
                 const { errno } = err;
@@ -29,19 +25,48 @@ class SchemasHandler {
     }
     private getSchemasFilesName(): Array<string> {
         this.ensureMigrationPathExists();
-        if (this.config.migrationsPath == null) throw new Error("No migrations path provided");
-        const schemas = readdirSync(this.config.migrationsPath).filter((path) =>
-            path.endsWith(".sql"),
-        );
+        if (this.migrationsPath == null) throw new Error("No migrations path provided");
+        const schemas = readdirSync(this.migrationsPath).filter((path) => path.endsWith(".sql"));
         return schemas;
     }
 
     private addMigrationPathToSchemasName(schemasFilesNames: Array<string>): Array<string> {
-        return schemasFilesNames.map((name) => this.config.migrationsPath + "/" + name);
+        return schemasFilesNames.map((name) => this.migrationsPath + "/" + name);
     }
 
     private readSQL(sqlPath: string) {
         return readFileSync(sqlPath);
+    }
+
+    private splitUpAndDownFromSQL(sql: string) {
+        const matches = sql.matchAll(this.upDownSeparatorRE);
+        let info = [];
+        for (const match of matches) {
+            const [text, direction] = match;
+            const { index } = match;
+            info.push({ text, direction, index });
+        }
+        const upInfo = info.flatMap((inf) => {
+            if (inf.direction !== "up") return [];
+            else return inf;
+        });
+        const downInfo = info.flatMap((inf) => {
+            if (inf.direction !== "down") return [];
+            else return inf;
+        });
+        const upIndexs = upInfo.map(({ index }) => index);
+        const downIndexs = downInfo.map(({ index }) => index);
+
+        const upCommentLen = upInfo[0].text.length;
+        const downCommentLen = downInfo[0].text.length;
+
+        upIndexs[0] = upIndexs[0] + upCommentLen;
+        downIndexs[0] = downIndexs[0] + downCommentLen;
+
+        const up = sql.slice(...upIndexs);
+        const down = sql.slice(...downIndexs);
+
+        return { up, down };
     }
 
     private combineSchemas(schemasPaths: Array<string>): string {
