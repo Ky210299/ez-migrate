1b4a386942d4a735e7cff214580bf9f5966ac0df
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-10 00:58:00 +0200

    ref: Migrations now only have migrate_at, up, down and path fields

--- src/Migration.ts
+++ src/Migration.ts
@@ -1,38 +1,35 @@
-import { randomUUID } from "node:crypto";
 export type MigrationData = {
-    id: string;
-    sql: string;
-    date: string;
+    migrated_at: string;
+    up: string;
+    down: string;
     path: string;
-    direction: 1 | 0;
-    orderNumber: number;
 };
 export default class Migration {
-    private readonly id: string;
-    private readonly sql: string;
-    private readonly date: string;
-    private readonly direction: 1 | 0; // 1 up and 0 down
+    private readonly migrated_at: string;
+    private readonly up: string;
+    private readonly down: string;
     private readonly path: string;
-    private readonly orderNumber: number;
 
-    constructor(data: Omit<MigrationData, "date" | "id">) {
-        const { sql, direction, path, orderNumber } = data;
-        if (!sql) throw new Error("Empty migration SQL");
-        else if (direction == null) throw new Error("Up or down must be specified");
+    constructor(data: Omit<MigrationData, "migrated_at">) {
+        const { up, down, path } = data;
+        if (!up) throw new Error('SQL fragment for "up" the migration, is missing');
+        else if (!down) throw new Error("Missing down migration");
         else if (!path) throw new Error("Migration path must be espeficied");
-        else if (orderNumber == null || orderNumber < 0) throw new Error("Invalid order number");
 
         const now = new Date().toISOString();
 
-        this.id = randomUUID();
-        this.sql = sql;
-        this.date = now;
-        this.direction = direction;
+        this.migrated_at = now;
+        this.up = up;
+        this.down = down;
         this.path = path;
-        this.orderNumber = orderNumber;
     }
 
-    getData() {
-        return { ...this };
+    getDetails() {
+        return {
+            migrated_at: this.migrated_at,
+            up: this.up,
+            down: this.down,
+            path: this.path,
+        };
     }
 }
