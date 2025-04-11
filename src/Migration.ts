9c6db0982814d0e1922568c273737856de196c13
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-11 02:50:40 +0200

    ref: add static getPreciseNow method to Migration class
    This is for use it in the SchemaHandler class when create migrations
    files

--- src/Migration.ts
+++ src/Migration.ts
@@ -10,18 +10,23 @@ export default class Migration {
     private readonly down: string;
     private readonly path: string;
 
+    /**
+     *  Concat now with a ns part that change in an acepted speed to avoid same timestamp when the
+     *  Migrations are instanciated in for-loops very fast
+     */
+    static getPreciseNow() {
+        const now = new Date().toISOString().substring(0, 23);
+        const ns = process.hrtime.bigint().valueOf() % BigInt(1e9); // Obtain the nanoseconds
+        return now.concat(`.${ns.toString().substring(3, 7)}`);
+    }
+
     constructor(data: Omit<MigrationData, "migratedAt">) {
         const { up, down, path } = data;
         if (!up) throw new Error('SQL fragment for "up" the migration, is missing');
         else if (!down) throw new Error("Missing down migration");
         else if (!path) throw new Error("Migration path must be espeficied");
 
-        const now = new Date().toISOString().substring(0, 23);
-        const ns = process.hrtime.bigint().valueOf() % BigInt(1e9); // Obtain the nanoseconds
-        /**
-            Concat now with a ns part that change in an acepted speed to avoid same timestamp when the Migrations are instanciated in for-loops very fast
-        */
-        this.migratedAt = now.concat(`.${ns.toString().substring(3, 7)}`);
+        this.migratedAt = Migration.getPreciseNow();
         this.up = up;
         this.down = down;
         this.path = path;
