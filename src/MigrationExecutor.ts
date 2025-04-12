75318046ccb29113063e220287827d115db9e2db
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-13 01:42:16 +0200

    fix: MigrationExecutor class, need refactor yet

--- src/MigrationExecutor.ts
+++ src/MigrationExecutor.ts
@@ -1,13 +1,59 @@
 import type DatabaseConnector from "./DatabaseConnector";
+import Migration from "./Migration";
+import Repository from "./Repository";
 
 class MigrationExecutor {
-    readonly dbconnector: DatabaseConnector;
-    constructor(dbconnector: DatabaseConnector) {
+    private readonly dbconnector: DatabaseConnector;
+    private readonly tracker: Repository
+    constructor(dbconnector: DatabaseConnector, tracker: Repository) {
         this.dbconnector = dbconnector;
+        this.tracker = tracker;
     }
-    async execute(sql: string) {
+    
+    async executeSingleMigrationUp(migration: Migration){
         await this.dbconnector.testConnection();
-        await this.dbconnector.runSQL(sql);
+        const migrationData = migration.getDetails()
+        const { commit, rollback } = await this.tracker.save([migrationData]);
+        try {
+            await this.dbconnector.runSQL(migrationData.up);
+            await commit()
+        } catch(err){
+           await rollback()
+           throw err;
+        }
+    }
+    async executeSingleMigrationDown(migration: Migration){
+        await this.dbconnector.testConnection();
+        const migrationData = migration.getDetails()
+        const { commit, rollback } = await this.tracker.save([migrationData]);
+        try {
+            await this.dbconnector.runSQL(migrationData.down);
+            await commit()
+        } catch(err){
+           await rollback()
+        }
+    };
+    async executeMigrationsUp(migrations: Array<Migration>) {
+        await this.dbconnector.testConnection();
+        const migrationsData = migrations.map(m => m.getDetails());
+        const { commit, rollback } = await this.tracker.save(migrationsData);
+        try {
+            await this.dbconnector.runSQL(migrationsData.map(m => m.up).join(""));
+            await commit()
+        } catch(err){
+           await rollback()
+        }
+    }
+    async executeMigrationsDown(migrations: Array<Migration>) {
+        await this.dbconnector.testConnection();
+        const migrationsData = migrations.map(m => m.getDetails());
+        const { commit, rollback } = await this.tracker.save(migrationsData);
+        try {
+            await this.dbconnector.runSQL(migrationsData.map(m => m.down).join(""));
+            await commit()
+        } catch (err) {
+            await rollback()
+        }
     }
 }
 
