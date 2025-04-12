b78b701a006c38b004bfbe4c501b9894bbe2b720
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-12 22:02:36 +0200

    fix: removing \n in up and down sql

--- src/SchemasHandler.ts
+++ src/SchemasHandler.ts
@@ -87,7 +87,7 @@ export default class SchemasHandler {
         if (!up.trim()) throw new Error("The UP migration section is empty");
         if (!down.trim()) throw new Error("The DOWN migration section is empty");
 
-        return { up, down };
+        return { up: up.replace(/\n/g, ""), down: down.replace(/\n/g, "") };
     }
 
     makeMigrationFile(name: string) {
