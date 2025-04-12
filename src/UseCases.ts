6b6a6388c15d17cb3f76fe296f62efdf1e116325
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-12 15:32:49 +0200

    feat: create empty UseCase class

---
+++ src/UseCases.ts
@@ -0,0 +1,11 @@
+export default class UseCases {
+    public static up() { }
+    public static down() { }
+    public static rollback() { }
+    public static migrate(path?: string) { }
+    public static status() { }
+    public static list() { }
+    public static reset() { }
+    public static version() { }
+    public static help() { }
+}
