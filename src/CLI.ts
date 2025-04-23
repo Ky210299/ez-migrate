f1388d321ae1ed47896adf053c1f937debb7bc32
Leonardo Bazán Márquez <leonardobazanmarquez@gmail.com>
2025-04-23 12:15:13 +0200

    feat: add make seed use case

--- src/CLI.ts
+++ src/CLI.ts
@@ -16,7 +16,16 @@ import Up from "./use-cases/Up";
 const program = new Command();
 
 program.name("ez-migrate").description("A simple migrations CLI tool").action(Migrate.run);
-program.command("make <name>").description("Create a new migration file").action(Make.run);
+program.command("make <name>")
+    .description("Create a new migration file")
+    .option("-s, --seed", "Create a seed file instead of a migration")
+    .action((name, options) => {
+        const { seed } = options
+        if (seed) {
+            MakeSeed.run(name) 
+        } else Make.run(name)
+    });
+program.command("seed").description("Run all seeds").action(Seed.run);
 program.command("down").description("Run down migration").action(Down.run);
 program.command("up").description("Run up migration").action(Up.run);
 program.command("init").description("Initialize migration setup").action(Init.run);
